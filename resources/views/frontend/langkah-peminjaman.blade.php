679e43da09ca502bec975f0e59710bc000a402d4
Aldy Sufriyanto <aldysufriyanto14@gmail.com>
2022-04-17 12:00:29 +0700

    add notif in cart

--- resources/views/frontend/langkah-peminjaman.blade.php
+++ resources/views/frontend/langkah-peminjaman.blade.php
@@ -24,45 +24,68 @@
                 <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/login.png')}}" alt="Card image cap">
                 <div class="card-body">
                     <h5><strong>Langkah 1</strong></h5>
-                    <p class="card-text text-justify">Login terlebih dahulu, yaitu dengan memasukan email dan kata sandi, untuk halaman login bisa di akses <a href="{{route('login')}}">disini</a> </p>
+                    <p class="card-text text-justify">Login terlebih dahulu, yaitu dengan memasukan email dan kata
+                        sandi, untuk halaman login bisa di akses <a href="{{route('login')}}">disini</a> </p>
                 </div>
             </div>
             <div class="card mt-3" style="width: 25rem;">
-                <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/home.png')}}" height="230px" alt="Card image cap">
+                <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/home.png')}}" height="230px"
+                    alt="Card image cap">
                 <div class="card-body">
                     <h5><strong>Langkah 2</strong></h5>
-                    <p class="card-text text-justify">Setelah berhasil login, maka akan menuju halaman utama pengguna, Kemudian pilih button cari barang, untuk mencari barang yang akan dipinjam.</p>
+                    <p class="card-text text-justify">Setelah berhasil login, maka akan menuju halaman utama pengguna,
+                        Kemudian pilih button cari barang, untuk mencari barang yang akan dipinjam.</p>
                 </div>
             </div>
             <div class="card mt-3" style="width: 25rem;">
-                <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/cari.png')}}" height="230px" alt="Card image cap">
+                <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/cari.png')}}" height="230px"
+                    alt="Card image cap">
                 <div class="card-body">
                     <h5><strong>Langkah 3</strong></h5>
-                    <p class="card-text text-justify">Gambar diatas adalah halaman daftar barang, Cari barang yang anda inginkan berdasarkan kategori laboratorium yang tersedia.</p>
+                    <p class="card-text text-justify">Gambar diatas adalah halaman daftar barang, Cari barang yang anda
+                        inginkan berdasarkan kategori laboratorium yang tersedia.</p>
                 </div>
             </div>
             <div class="card mt-3" style="width: 25rem;">
-                <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/form pengajuan.png')}}" height="230px" alt="Card image cap">
+                <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/form pengajuan.png')}}" height="230px"
+                    alt="Card image cap">
                 <div class="card-body">
                     <h5><strong>Langkah 4</strong></h5>
-                    <p class="card-text text-justify">Setelah memilih barang yang diinginkan, maka akan menuju halaman detail, pada halaman ini anda diwajibkan mengisi informasi seperti jumlah alat, keperluaan alat, tanggal pinjam dan tanggal kembali.</p>
+                    <p class="card-text text-justify">Setelah memilih barang yang diinginkan, maka akan menuju halaman
+                        detail, pada halaman ini anda diwajibkan mengisi informasi seperti jumlah alat, keperluaan alat,
+                        tanggal pinjam dan tanggal kembali.</p>
                 </div>
             </div>
             <div class="card mt-3" style="width: 25rem;">
-                <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/keranjang.png')}}" height="230px" alt="Card image cap">
+                <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/keranjang.png')}}" height="230px"
+                    alt="Card image cap">
                 <div class="card-body">
                     <h5><strong>Langkah 5</strong></h5>
-                    <p class="card-text text-justify">Setelah mengisi informasi peminjaman, Peminjam akan diarahkan ke halaman keranjang, Gambar diatas adalah halaman keranjang, yang mana terdapat daftar barang yang dipilih, tunggu hingga status pengajuan disetujui operator laboratorium terkait.</p>
+                    <p class="card-text text-justify">Setelah mengisi informasi peminjaman, Peminjam akan diarahkan ke
+                        halaman keranjang, Gambar diatas adalah halaman keranjang, yang mana terdapat daftar barang yang
+                        dipilih, tunggu hingga status pengajuan disetujui operator laboratorium terkait.</p>
                 </div>
             </div>
             <div class="card mt-3" style="width: 25rem;">
-                <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/print.png')}}" height="230px" alt="Card image cap">
+                <img class="card-img-top p-2" src="{{asset('frontend/img/langkah/print.png')}}" height="230px"
+                    alt="Card image cap">
                 <div class="card-body">
                     <h5><strong>Langkah 6</strong></h5>
-                    <p class="card-text text-justify">Setelah status peminjaman disetujui, maka peminjam harus melakukan cetak surat peminjaman untuk melakukan aktivasi peminjaman barang ke operator laboratorium terkait.</p>
+                    <p class="card-text text-justify">Setelah status peminjaman disetujui, maka peminjam harus melakukan
+                        unduh surat peminjaman untuk melakukan aktivasi peminjaman barang ke operator laboratorium
+                        terkait.</p>
                 </div>
             </div>
+            <div class="card mt-3" style="width: 25rem;">
+                <center><img class="card-img-top p-2" src="{{asset('frontend/img/langkah/surat.png')}}" height="230px"
+                        style="width: 250px" alt="Card image cap">
+                    <div class="card-body">
+                </center>
+                <h5><strong>Langkah 7</strong></h5>
+                <p class="card-text text-justify">Setelah melakukan unduh surat peminjaman, peminjam harus menunjukkan surat tersebut kepada operator laboratorium terkait untuk dilakukan aktivasi peminjaman dan pengambilan barang pinjaman</p>
+            </div>
         </div>
+    </div>
     </div>
 </main>
 @endsection
