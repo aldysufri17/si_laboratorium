0273ba589888a72dca89717df94a930b54676d51
Aldy Sufriyanto <aldysufriyanto14@gmail.com>
2022-06-06 09:58:29 +0700

    rollback scan

--- resources/views/backend/transaksi/konfirmasi/peminjaman/scan.blade.php
+++ resources/views/backend/transaksi/konfirmasi/peminjaman/scan.blade.php
@@ -1,4 +1,4 @@
-{{-- @extends('backend.layouts.app')
+@extends('backend.layouts.app')
 
 @section('title', 'Scan Pengembalian')
 
@@ -71,35 +71,4 @@
             console.error(e);
         });
 </script>
-@endsection --}}
-
-<!DOCTYPE html>
-<html>
-  <head>
-    <title>Instascan</title>
-    <script type="text/javascript" src="https://rawgit.com/schmich/instascan-builds/master/instascan.min.js" ></script>	
-  </head>
-  <body>
-    <video id="preview"></video>
-    <script>
-        let scanner = new Instascan.Scanner(
-            {
-                video: document.getElementById('preview')
-            }
-        );
-        scanner.addListener('scan', function(content) {
-            alert('Escaneou o conteudo: ' + content);
-            window.open(content, "_blank");
-        });
-        Instascan.Camera.getCameras().then(cameras => 
-        {
-            if(cameras.length > 0){
-                scanner.start(cameras[0]);
-            } else {
-                console.error("Não existe câmera no dispositivo!");
-            }
-        });
-    </script>
-
- </body>
-</html>
+@endsection
